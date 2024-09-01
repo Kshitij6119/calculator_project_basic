368fee485adccd7bd96184c0c19f2c3a0d1bdf58
Kshitij6119 <134214296+Kshitij6119@users.noreply.github.com>
2024-09-01 15:43:21 +0530

    Add files via upload

---
+++ calulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calulatorApp.swift
+//  calulator
+//
+//  Created by Kshitij Singh on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
